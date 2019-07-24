6dd8a82e3a36a54ca59e46f44352f160d23b4912
Aksal <aksal.sf@gmail.com>
2019-07-24 14:43:04 +0700

    start

---
+++ dolaptop/admin/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dol Laptop</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="assets/js/jquery-3.3.1.slim.min.js"></script>
+	<script src="assets/js/popper.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+ 
+
+</head>
+<body>
+	<!-- Navbar -->
+    <nav class="navbar navbar-expand-lg bg-white navbar-light shadow-sm fixed-top">
+	    <div class="container">
+	    <!-- Brand -->
+	      <a class="navbar-brand" href="#">Dolaptop</a>
+	    <!-- Toggler -->
+	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="navbar-toggler-icon"></span>
+	      </button>
+	    <!-- Content Nav -->
+	      <div class="collapse navbar-collapse" id="navbar">
+	        <ul class="navbar-nav mr-auto">
+	          <li class="nav-item">
+	            <a class="nav-link" href="#">Beranda</a>
+	          </li>
+	          <li class="nav-item">
+	          	<a class="nav-link" href="#">Stok Barang</a>
+	          </li>
+	          <li class="nav-item">
+	          	<a class="nav-link" href="#">Transaksi</a>
+	          </li>
+	        </ul>
+	      </div>
+	    </div>
+    </nav>
+    <!-- Navbar End -->
+</body>
+</html>
